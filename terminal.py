f09b205a38845ba56bdd12769135aebf3f87b873
BrycenAsay <brycen1177@gmail.com>
2025-02-01 00:19:20 -0700

    Created '-help' and '?' terminal commands to help to inhance terminal interactability and ease of use. Also added fourth terminal command attribute as part of this feature addition

--- terminal.py
+++ terminal.py
@@ -1,6 +1,7 @@
 from account_creation import create_creds, reset_password, aws_login
 from s3 import sel_bucket, mk_bucket, del_bucket_ap, updt_bucket_ap, ls_bucket, bucketSettings
 from s3_bucket import upload_object
+import logging
 
 class terminal:
     """terminal class handles storage and execution of terminal commands especially as it relates
@@ -8,54 +9,103 @@ class terminal:
     # __init__ command values,  set each to none until a value is specified
     def __init__(self,
                  terminal_type,
-                 cmd_prop_order = ['first_cmd', 'second_cmd', 'third_cmd'], 
+                 cmds_help_dict = {'-res': 'Used as the reset command',
+                               '-srv': 'Used as the access service command',
+                               '-sel': 'Used as the select command',
+                               '-mk': 'Used as the make command',
+                               '-del': 'Used as the delete command',
+                               '-updt': 'Used as the update command',
+                               '-ls': 'Used as the list command',
+                               'bucket_settings': 'Provides a list of settings that are set on an s3 bucket',
+                               '-upld': 'Used as the upload command',
+                               'password': 'Used in comibnation with a command to make changes to a password',
+                               's3': 'A simple storage service using s3 buckets and bucket objects',
+                               'aws': 'The very first keyword used to access the application in combination with other commands',
+                               'login': 'Used to log into the AWS application',
+                               'user': 'Used in combination with other commands to control users',
+                               'create': 'Used as the create command',
+                               'bucket': 'Used in combination with otehr commands to control s3 buckets, typically you would perceed this keyword with the name of an existing bucket'},
+                 cmd_prop_order = ['first_cmd', 'second_cmd', 'third_cmd', 'fourth_cmd'], 
                  first_cmd=None,
                  second_cmd=None,
-                 third_cmd=None):
+                 third_cmd=None,
+                 fourth_cmd=None):
+        self.cmds_help_dict = cmds_help_dict
         self.terminal_type = terminal_type
         self.cmd_prop_order = cmd_prop_order
         self.first_cmd = first_cmd
         self.second_cmd = second_cmd
         self.third_cmd = third_cmd
+        self.fourth_cmd = fourth_cmd
     
-    def set_commands(self, attr, value): #sets command values for terminal object
+    def set_commands(self, attr, value): 
+        """sets command values for terminal object"""
         if attr == 'first_cmd':
             self.first_cmd = value
         elif attr == 'second_cmd':
             self.second_cmd = value
         elif attr == 'third_cmd':
             self.third_cmd = value
+        elif attr == 'fourth_cmd':
+            self.fourth_cmd = value
 
-    def get_commands(self, attr): #returns stored command values for terminal instances
+    def get_commands(self, attr): 
+        """returns stored command values for terminal instances"""
         if attr == 'first_cmd':
             return self.first_cmd
         elif attr == 'second_cmd':
             return self.second_cmd
         elif attr == 'third_cmd':
             return self.third_cmd
+        elif attr == 'fourth_cmd':
+            return self.fourth_cmd
 
-    def run_cmds(self, cmds, param_list:list=[]): #executes each command in order until it reaches None or a callable function
-        for cmd in self.cmd_prop_order:
-                if self.get_commands(cmd) is not None:
-                    cmds = cmds[self.get_commands(cmd)]
-                    if callable(cmds):
-                        cmds(*param_list) #If cmd returns a callable function, call the function
+    def question_command(self, _cmds):
+        list(map(lambda x: print(x), [key for key in _cmds]))
 
-    def term_spec_cmds(self, term, param_list:list=[]): 
+    def help_command(self, raw_cmds):
+        """Baked in terminal command which provides what a given set of commands does"""
+        cpo = self.cmd_prop_order
+        if '-help' in raw_cmds:
+            cmd_lineage = []
+            for key_num in range(raw_cmds.index('-help')):
+                cmd_lineage.append(f'{self.get_commands(cpo[key_num])}: {self.cmds_help_dict[self.get_commands(cpo[key_num])]}')
+            joined_explanation = " --> ".join(cmd_lineage)
+            print(f'{joined_explanation}')
+            return True
+        else:
+            return False
+
+    def run_cmds(self, cmds, param_list:list=[]): 
+        """executes each command in order until it reaches None or a callable function"""
+        cpo = self.cmd_prop_order
+        needs_help = self.help_command([self.get_commands(x) for x in self.cmd_prop_order])
+        for cmd_num in range(len(cpo)):
+            if needs_help:
+                pass
+            elif self.get_commands(cpo[cmd_num]) == '?':
+                self.question_command(cmds)
+            elif self.get_commands(cpo[cmd_num]) is not None:
+                cmds = cmds[self.get_commands(cpo[cmd_num])]
+                if callable(cmds):
+                    cmds(*param_list) #If cmd returns a callable function, call the function
+
+    def term_spec_cmds(self, param_list:list=[]): 
         """command heirchey specified by terminal"""
-        if term == 'ap':
-            self.run_cmds({'aws': {'login': aws_login, 'create': {'user': create_creds}}}, param_list)
-        elif term == 'acct':
+        if self.terminal_type == 'ap':
+            self.run_cmds({'aws': {'login': aws_login, 
+                                   'create': {'user': create_creds}}}, param_list)
+        elif self.terminal_type == 'acct':
             self.run_cmds({'-res': {'password': reset_password}, 
                            '-srv': {'s3': s3_def_terminal}}, param_list)
-        elif term == 's3':
+        elif self.terminal_type == 's3':
             self.run_cmds({'-sel': {'bucket': sel_bucket},
                            '-mk': {'bucket': mk_bucket},
                            '-del': {'bucket': del_bucket_ap},
                            '-updt': {'bucket': updt_bucket_ap},
                            '-ls': {'bucket': ls_bucket},
                            'bucket_settings': bucketSettings}, param_list)
-        elif term == 's3_bucket':
+        elif self.terminal_type == 's3_bucket':
             self.run_cmds({'-upld': upload_object}, param_list)
 
 def dynamic_term_vals(terminal_type, _term_cmd_obj, _term_spec_parm): 
@@ -74,7 +124,7 @@ def terminal_entry(_terminal_type, cmd_str, term_spec_parm:list=[]):
         if i <= len(term_cmd_obj.cmd_prop_order) - 1:
             term_cmd_obj.set_commands(term_cmd_obj.cmd_prop_order[i], cmd_list[i])
     try:
-        term_cmd_obj.term_spec_cmds(_terminal_type, dynamic_term_vals(_terminal_type, term_cmd_obj, term_spec_parm))
+        term_cmd_obj.term_spec_cmds(dynamic_term_vals(_terminal_type, term_cmd_obj, term_spec_parm))
     except:
         pass
 
@@ -109,3 +159,7 @@ def s3_bucket_terminal(_username, bucket):
         terminal_entry('s3_bucket', prompt_command, [_username, bucket, None])
         prompt_command = input(f'AWS\\users\\{_username}\\s3\\{bucket.name}> ')
     s3_def_terminal(_username)
+
+if __name__ == '__main__':
+    yes = ' --> '.join(['yes', 'no', 'maybe so'])
+    print(f'{yes}')
