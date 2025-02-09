7d0e7e74a1ab12f0e4ed44297820f1fd2966e307
BrycenAsay <brycen1177@gmail.com>
2025-02-09 13:22:34 -0700

    Updated terminal object to handle dynamic X number of arguments as opposed to statically accepting four command arguments to allow for auto-scaling of number of command line arguments

--- terminal.py
+++ terminal.py
@@ -25,51 +25,33 @@ class terminal:
                                'user': 'Used in combination with other commands to control users',
                                'create': 'Used as the create command',
                                'bucket': 'Used in combination with otehr commands to control s3 buckets, typically you would perceed this keyword with the name of an existing bucket'},
-                 cmd_prop_order = ['first_cmd', 'second_cmd', 'third_cmd', 'fourth_cmd'], 
-                 first_cmd=None,
-                 second_cmd=None,
-                 third_cmd=None,
-                 fourth_cmd=None):
+                 cmds=None):
         self.cmds_help_dict = cmds_help_dict
         self.terminal_type = terminal_type
-        self.cmd_prop_order = cmd_prop_order
-        self.first_cmd = first_cmd
-        self.second_cmd = second_cmd
-        self.third_cmd = third_cmd
-        self.fourth_cmd = fourth_cmd
-    
-    def set_commands(self, attr, value): 
-        """sets command values for terminal object"""
-        if attr == 'first_cmd':
-            self.first_cmd = value
-        elif attr == 'second_cmd':
-            self.second_cmd = value
-        elif attr == 'third_cmd':
-            self.third_cmd = value
-        elif attr == 'fourth_cmd':
-            self.fourth_cmd = value
+        if cmds == None:
+            cmds = []
+        self.cmds = cmds
 
-    def get_commands(self, attr): 
+    def set_commands(self, value): 
+        """sets command values for terminal object"""
+        self.cmds.append(value)
+
+    def get_commands(self, attr_num): 
         """returns stored command values for terminal instances"""
-        if attr == 'first_cmd':
-            return self.first_cmd
-        elif attr == 'second_cmd':
-            return self.second_cmd
-        elif attr == 'third_cmd':
-            return self.third_cmd
-        elif attr == 'fourth_cmd':
-            return self.fourth_cmd
+        try:
+            return self.cmds[attr_num]
+        except:
+            return None
 
     def question_command(self, _cmds):
         list(map(lambda x: print(x), [key for key in _cmds]))
 
     def help_command(self, raw_cmds):
         """Baked in terminal command which provides what a given set of commands does"""
-        cpo = self.cmd_prop_order
         if '-help' in raw_cmds:
             cmd_lineage = []
             for key_num in range(raw_cmds.index('-help')):
-                cmd_lineage.append(f'{self.get_commands(cpo[key_num])}: {self.cmds_help_dict[self.get_commands(cpo[key_num])]}')
+                cmd_lineage.append(f'{self.get_commands(key_num)}: {self.cmds_help_dict[self.get_commands(key_num)]}')
             joined_explanation = " --> ".join(cmd_lineage)
             print(f'{joined_explanation}')
             return True
@@ -78,15 +60,14 @@ class terminal:
 
     def run_cmds(self, cmds, param_list:list=[]): 
         """executes each command in order until it reaches None or a callable function"""
-        cpo = self.cmd_prop_order
-        needs_help = self.help_command([self.get_commands(x) for x in self.cmd_prop_order])
-        for cmd_num in range(len(cpo)):
+        needs_help = self.help_command([self.get_commands(x) for x in range(len(self.cmds))])
+        for cmd_num in range(len(self.cmds)):
             if needs_help:
                 pass
-            elif self.get_commands(cpo[cmd_num]) == '?':
+            elif self.get_commands(cmd_num) == '?':
                 self.question_command(cmds)
-            elif self.get_commands(cpo[cmd_num]) is not None:
-                cmds = cmds[self.get_commands(cpo[cmd_num])]
+            elif self.get_commands(cmd_num) is not None:
+                cmds = cmds[self.get_commands(cmd_num)]
                 if callable(cmds):
                     cmds(*param_list) #If cmd returns a callable function, call the function
 
@@ -113,18 +94,17 @@ class terminal:
 def dynamic_term_vals(terminal_type, _term_cmd_obj, _term_spec_parm): 
     """function that sets dynamic cmd line arguments into the terminal instance (ex: name of a bucket or bucket object)"""
     if terminal_type == 's3':
-        _term_spec_parm[1] = _term_cmd_obj.third_cmd
+        _term_spec_parm[1] = _term_cmd_obj.get_commands(2)
     elif terminal_type == 's3_bucket':
-        _term_spec_parm[2] = _term_cmd_obj.second_cmd
+        _term_spec_parm[2] = _term_cmd_obj.get_commands(1)
     return _term_spec_parm
 
 def terminal_entry(_terminal_type, cmd_str, term_spec_parm:list=[]): 
     """entry point to instantiate a terminal instance, parameters include terminal type, cmd propmt string, and terminal specific function arguments"""
     term_cmd_obj = terminal(_terminal_type)
     cmd_list = cmd_str.split(' ')
-    for i in range(len(cmd_list)):
-        if i <= len(term_cmd_obj.cmd_prop_order) - 1:
-            term_cmd_obj.set_commands(term_cmd_obj.cmd_prop_order[i], cmd_list[i])
+    for cmd in cmd_list:
+        term_cmd_obj.set_commands(cmd)
     try:
         term_cmd_obj.term_spec_cmds(dynamic_term_vals(_terminal_type, term_cmd_obj, term_spec_parm))
     except:
@@ -163,5 +143,4 @@ def s3_bucket_terminal(_username, bucket):
     s3_def_terminal(_username)
 
 if __name__ == '__main__':
-    yes = ' --> '.join(['yes', 'no', 'maybe so'])
-    print(f'{yes}')
+    ap_level_main()
