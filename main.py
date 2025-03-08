a56a98d16353d360a03a5aaa41a0f48bbe5cc70a
BrycenAsay <brycen1177@gmail.com>
2025-03-08 00:34:50 -0700

    Moved files to different subdirectories in order to better orchastraite Docker

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from terminal import ap_level_main
-
-def main():
-    # main entrypoint function, upon running goes into access point terminal
-    ap_level_main()
-    
-if __name__ == '__main__':
-    main()
