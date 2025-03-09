cefa8bda33ac0b7f3fcf2c060438b9a951700aac
BrycenAsay <brycen1177@gmail.com>
2025-03-09 00:59:46 -0700

    Updated SQL to include lifecycle_transition table and account for the fact that lifecycle ids are specified in s3 not s3_bucket

--- db/service_maintenance/lifecycle_transition.sql
+++ db/service_maintenance/lifecycle_transition.sql
@@ -1,8 +1,17 @@
 
-UPDATE s3_bucket SET storage_class = cycr.transition_to
+UPDATE s3_bucket SET storage_class = cyct.transition_to
 
-FROM lifecycle_rule cycr
-WHERE s3_bucket.user_id = cycr.user_id
-AND s3_bucket.bucket_id = cycr.bucket_id
-AND s3_bucket.lifecycle_id = cycr.lifecycle_id
-AND current_date::date - (SELECT creation_date::date FROM s3_bucket) = days_till_transition
+FROM s3
+
+INNER JOIN lifecycle_rule cycr
+ON cycr.user_id = s3.user_id
+AND cycr.bucket_id = s3.bucket_id
+AND cycr.lifecycle_id = s3.lifecycle_id
+
+INNER JOIN lifecycle_transition cyct
+ON cycr.lifecycle_id = cyct.lifecycle_id
+
+WHERE s3_bucket.user_id = s3.user_id
+AND s3_bucket.bucket_id = s3.bucket_id
+AND current_date::date - (SELECT creation_date::date FROM s3_bucket) = cyct.days_till_transition
+AND cycr.rule_enabled = True
