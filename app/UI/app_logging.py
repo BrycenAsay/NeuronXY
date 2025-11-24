680f08b913487e8c21569a70a4e9b856dbd7dfd3
BrycenAsay <brycen1177@gmail.com>
2025-11-23 17:25:42 -0700

    Fixed small bug as it related to host/object specification in the log

--- app/UI/app_logging.py
+++ app/UI/app_logging.py
@@ -27,6 +27,7 @@ def create_log_entry(_username, action, description, srv_name=None, host_name=No
     object_json = create_object_json(raw_object)
     log_data = postgres_format([log_timestamp, description, user_id, srv_name, action, host_name, host_json, object_name, object_json, False])
     create_db_connection(create_row('logging', cols, log_data))
-    if (srv_name == 'cortex' and (create_db_connection(raw_sql(f'SELECT COUNT(*) FROM synapse_qf WHERE in_srv_id = {name_to_id('cortex', 'node_id', 'nrn', getattr(raw_host, 'nrn'))}'), return_result=True)[0] > 0 and not synapse_run)):
-        synapse_log_reader(user_id, srv_name, action, host_name, object_name)
+    if raw_host is not None:
+        if (srv_name == 'cortex' and (create_db_connection(raw_sql(f'SELECT COUNT(*) FROM synapse_qf WHERE in_srv_id = {name_to_id('cortex', 'node_id', 'nrn', getattr(raw_host, 'nrn'))}'), return_result=True)[0] > 0 and not synapse_run)):
+            synapse_log_reader(user_id, srv_name, action, host_name, object_name)
     create_db_connection(row_action('', ['synapse_processed'], ['False'], f'UPDATE logging SET synapse_processed = True', frm_keywrd=''))
